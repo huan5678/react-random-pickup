2ff2e905a4c8e09f71d2bd192e3494433eb6ddce
huan5678 <huan5678@gmail.com>
2024-04-29 17:00:48 +0800

    重構: 重構 `Config.tsx` 中的 UI 佈局和組件
    - 修改 `src/App.tsx` 中的部分容器類別
    - 在 `src/components/Config.tsx` 中新增 `useWindowSize` 導入
    - 在 `src/components/Config.tsx` 中新增 `Separator` 組件
    - 在 `src/components/Config.tsx` 中新增 `width` 常數初始化
    - 在 `src/components/Config.tsx` 中新增控制台日誌語句
    - 更新 `src/components/Config.tsx` 中的 UI 佈局
    
    Signed-off-by: huan5678 <huan5678@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -115,91 +115,93 @@ function App() {
         </h1>
         <h2 className="text-3xl font-bold text-center">{subTitle}</h2>
       </div>
-      <section className="container flex flex-col justify-between gap-4 px-4 py-8 bg-background rounded-2xl">
-        {selectedStrings.length === 0 && (
-          <div className="flex flex-col items-end gap-2">
-            <p>目前題目數: {dataList.filter((q) => !q.selected).length}</p>
-            <Button
-              type="button"
-              onClick={startPickup}
-              disabled={dataList.length <= drawCount || isSpinning}
-            >
-              隨機抽選
-            </Button>
-          </div>
-        )}
-        {isSpinning && (
-          <StringSpinner strings={dataList} drawCount={drawCount} />
-        )}
-        <div className="flex flex-col w-full gap-4 mx-auto">
-          <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle className="text-4xl">抽選結果</DialogTitle>
-              </DialogHeader>
-              <DialogDescription>獲選的是:</DialogDescription>
-              <ul>
-                {selectedStrings.map((item) => (
-                  <li className="text-2xl" key={item.name}>
-                    {item.name}
-                  </li>
-                ))}
-              </ul>
-            </DialogContent>
-          </Dialog>
-          {selectedStrings.length > 0 && (
-            <div>
-              <h3 className="mb-2 text-2xl text-center">本次獲選的是</h3>
-              <Separator />
-              <ul className="flex flex-col max-w-screen-lg gap-4 mx-auto mb-2">
-                {selectedTransitions((style, string) => (
-                  <animated.li
-                    key={string.name}
-                    style={style}
-                    className={`px-6 py-4 text-lg md:text-6xl text-center bg-primary rounded-xl text-primary-foreground`}
-                  >
-                    {string.name}
-                  </animated.li>
-                ))}
-              </ul>
-              <div className="flex justify-end w-full">
-                <Button
-                  type="button"
-                  variant="outline"
-                  onClick={() => handleClearSelectStrings()}
-                >
-                  清空
-                </Button>
-              </div>
+      <section className="container max-w-screen-2xl">
+        <div className="flex flex-col justify-between gap-4 px-4 py-8 bg-background rounded-2xl">
+          {selectedStrings.length === 0 && (
+            <div className="flex flex-col items-end gap-2">
+              <p>目前題目數: {dataList.filter((q) => !q.selected).length}</p>
+              <Button
+                type="button"
+                onClick={startPickup}
+                disabled={dataList.length <= drawCount || isSpinning}
+              >
+                隨機抽選
+              </Button>
             </div>
           )}
-        </div>
-        {dataList.filter((q) => q.selected).length > 0 && (
-          <div className="flex justify-end">
-            <Dialog>
-              <DialogTrigger>
-                <div className="flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md whitespace-nowrap ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90">
-                  察看歷史項目
-                </div>
-              </DialogTrigger>
+          {isSpinning && (
+            <StringSpinner strings={dataList} drawCount={drawCount} />
+          )}
+          <div className="flex flex-col w-full gap-4 mx-auto">
+            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
               <DialogContent>
                 <DialogHeader>
-                  <DialogTitle className="text-4xl">已選擇的項目</DialogTitle>
+                  <DialogTitle className="text-4xl">抽選結果</DialogTitle>
                 </DialogHeader>
-                <DialogDescription>目前已抽選過的項目:</DialogDescription>
-                <ul className="space-y-2">
-                  {dataList
-                    .filter((q) => q.selected)
-                    .map((item) => (
-                      <li className="text-2xl" key={item.name}>
-                        {item.name}
-                      </li>
-                    ))}
+                <DialogDescription>獲選的是:</DialogDescription>
+                <ul>
+                  {selectedStrings.map((item) => (
+                    <li className="text-2xl" key={item.name}>
+                      {item.name}
+                    </li>
+                  ))}
                 </ul>
               </DialogContent>
             </Dialog>
+            {selectedStrings.length > 0 && (
+              <div>
+                <h3 className="mb-2 text-2xl text-center">本次獲選的是</h3>
+                <Separator />
+                <ul className="flex flex-col max-w-screen-lg gap-4 mx-auto mb-2">
+                  {selectedTransitions((style, string) => (
+                    <animated.li
+                      key={string.name}
+                      style={style}
+                      className={`px-6 py-4 text-lg md:text-6xl text-center bg-primary rounded-xl text-primary-foreground`}
+                    >
+                      {string.name}
+                    </animated.li>
+                  ))}
+                </ul>
+                <div className="flex justify-end w-full">
+                  <Button
+                    type="button"
+                    variant="outline"
+                    onClick={() => handleClearSelectStrings()}
+                  >
+                    清空
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+          {dataList.filter((q) => q.selected).length > 0 && (
+            <div className="flex justify-end">
+              <Dialog>
+                <DialogTrigger>
+                  <div className="flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors rounded-md whitespace-nowrap ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90">
+                    察看歷史項目
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-4xl">已選擇的項目</DialogTitle>
+                  </DialogHeader>
+                  <DialogDescription>目前已抽選過的項目:</DialogDescription>
+                  <ul className="space-y-2">
+                    {dataList
+                      .filter((q) => q.selected)
+                      .map((item) => (
+                        <li className="text-2xl" key={item.name}>
+                          {item.name}
+                        </li>
+                      ))}
+                  </ul>
+                </DialogContent>
+              </Dialog>
+            </div>
+          )}
+        </div>
       </section>
       <div className="container absolute bottom-0 right-0 flex items-center justify-end w-full max-w-lg gap-4 mb-2 text-white -translate-x-4 -translate-y-4 opacity-25">
         <Config />
